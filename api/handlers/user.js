69f14a42bbeb3e5a9951933f8f7adc1b8dd81760
Keshav Bagla <keshavbagla@gmail.com>
2017-03-04 23:56:59 -0500

    added mongo connection

--- api/handlers/user.js
+++ api/handlers/user.js
@@ -1,4 +1,6 @@
 const Boom = require('Boom');
+const uuid = require('node-uuid');
+
 module.exports = {
   register: (request, reply) => {
     reply(Boom.notImplemented());
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const Hapi = require('hapi');
-const server = new Hapi.Server();
+const mongojs = require('mongojs');
 const user = require('./api/routes/userRoutes');
+
+const server = new Hapi.Server();
 
 server.connection({
   port:3000,
   host: 'localhost'
-})
+});
+
+server.app.db = mongojs('http://localhost:27017', ['users', 'rooms']);
 
 server.route(user)
 server.start((err) => {
