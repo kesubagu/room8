5fc1ef016338f3ff99d905b2c6320ea473a13e74
Keshav Bagla <keshavbagla@gmail.com>
2017-03-25 19:34:26 -0400

    added new model

---
+++ models/Grocery.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Room = require('./Room')
+const db = require('../middleware/database');
+
+var Schema = mongoose.Schema;
+
+var GrocerySchema = new Schema({
+  items: [
+    {
+      name: {
+        type: 'String',
+        required: true
+      },
+      putBy: {
+        type: Schema.Types.ObjectId,
+        required: true
+      },
+      boughtBy: {
+        type: Schema.Types.ObjectId,
+        default: null;
+      }
+    }
+  ],
+  roomId: {type: Schema.Types.ObjectId, ref: 'Room'}
+});
+
+var Grocery = db.model('Grocery', GrocerySchema);
+
+module.exports = Grocery;
